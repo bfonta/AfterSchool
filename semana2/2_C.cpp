378884b6c4c2fd67ad64d8559aed5b3e72d73aa3
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-10-24 01:32:16 +0200

    :white_check_mark: vector para guardar calculos intermedios

--- semana2/2_C.cpp
+++ semana2/2_C.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+using lli = long long int;
 
 int guimaracci(int n) {
    if(n == 1)
@@ -9,10 +11,26 @@ int guimaracci(int n) {
 	 return guimaracci(n-1) + guimaracci(n-2);
 }
 
+lli guimaracci2(std::vector<lli>& vec, int n) {
+  if(vec[n-1]!=0)
+	return vec[n-1];
+
+  if(vec[n-3]==0)
+	vec[n-3] = guimaracci2(vec, n-2);
+  if(vec[n-2]==0)
+	vec[n-2] = guimaracci2(vec, n-1);
+
+  return vec[n-1] = vec[n-3] + vec[n-2];
+}
+
 // C. Os numeros de Guimaracci
 int main() {
   int N;
   std::cin >> N;
-  std::cout << guimaracci(N) << std::endl;
+
+  std::vector<lli> vec(N, 0);
+  vec[0] = 1;
+  vec[1] = 3;
+  std::cout << guimaracci2(vec, N) << std::endl;
   return 0;
 }
