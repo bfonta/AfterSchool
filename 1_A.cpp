dee1616b72faf4cea1ad537e095a031b65cd5102
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-10-23 16:39:44 +0200

    :white_check_mark: problema A resolvido

--- 1_A.cpp
+++ 1_A.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <assert.h>
 
 int main() {
-
+  std::string name = "";
+  std::cin >> name;
+  assert( name.length() <= 10 );
+  std::cout << "Bom dia " + name + "!" << std::endl;
   return 0;
 }
