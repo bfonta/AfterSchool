524a3fc8520442ba21e2eefc6a74ea1df7e912f7
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-11-02 20:00:00 +0100

    :white-check-mark: problema 3A resolvido

--- semana3/3_A.cpp
+++ semana3/3_A.cpp
@@ -1,31 +1,20 @@
 #include<iostream>
-
+#define ll long long
 
 int to_digit(char x) { return (int)x - '0'; }
 
 //A. New Skateboard
 int main() {
-  //read input array
-  int Ntot = 3e5;
-  char input[Ntot];
-  scanf("%s", input);
-
-  //get input array size
-  int N=0;
-  for(int i=0; i<Ntot; ++i) {
-	if(input[i]=='\0') {
-	  N=i;
-	  break;
-	}
-  }
-
+  std::string input;
+  std::cin >> input;
+  ll N = input.length();
+	
   //count substrings that are multiples of 4
-  long long contador=0;
+  ll contador=0, digit1=-1, digit2=-1;
   for(int j=N-1; j>=0; --j) {
-	long long digit1 = to_digit(input[j]);
-	long long digit2 = -1;
+	digit1 = to_digit(input[j]);
 	if(digit1%4 == 0)
-	  contador += 1;
+	  contador++;
 
 	if(j>0) {
 	  digit2 = to_digit(input[j-1]);
