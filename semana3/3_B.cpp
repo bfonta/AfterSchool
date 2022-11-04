7d148449d900170b27978bfccb1f6e3bb2da87fb
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-11-04 09:04:05 +0100

    :white_check_mark: problema 3B resolvido

---
+++ semana3/3_B.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int to_digit(char x) { return (int)x - '0'; }
+
+//A. Sequencia de parenteses
+int main() {
+  string in;
+  cin >> in;
+
+  stack<char> pilha;
+  for(int i=0; i<in.size(); ++i) 
+	{
+	  if(pilha.empty() and in[i] == ')') {
+		printf("0\n");
+		return 0;
+	  }
+
+	  if(in[i]=='(')
+		pilha.push(in[i]);
+	  else
+		pilha.pop();
+	}
+
+  printf("%d\n", pilha.empty());
+  
+  return 0;
+}
