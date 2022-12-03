f90d7f867269a8c90b66532f7147a0958ac54a67
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-12-03 09:41:24 +0100

    :white_check_mark: 5A

--- semana5/5_A.cpp
+++ semana5/5_A.cpp
@@ -1,46 +1,43 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+using ll = long long;
 
-int linear(const vector<int> &arr, int pot) {
-  const int s = arr.size();
-  for(int i = 0; i<s; ++i) 
-	{
-	  if(pot<arr[i])
-		return i;
-	}
-  return s;
-}
-
-int logar(const vector<int> &arr, int pot) {
-  int iold = 1, itmp, extra;
-  int inew = arr.size()/2;
+int logar(const vector<ll> &arr, ll val) {
+  ll iold=0, dist=-1, itmp;
+  ll inew = arr.size()/2+1;
   while(true)
 	{
-	  if(iold==inew) break;
+	  if(inew<1) inew=1;
+	  if(inew>arr.size()) inew=arr.size();
 	  
-	  //extra = abs(inew-iold)==1 ? 1 : 0;
-	  extra = 0;
-	  if(pot<arr[inew-1])
-		itmp = inew - abs(inew-iold)/2 - extra;
+	  if(iold==inew or val==arr[inew-1]) break;
+	  
+	  dist = abs(inew-iold);
+	  if(dist%2==1 and dist>2)
+		++dist;
+	  dist /= 2;
+	  //cout << dist << " " << iold << " " << inew << endl;
+	  if(val<arr[inew-1])
+		itmp = inew - max<ll>(dist,1);
 	  else
-		itmp = inew + abs(inew-iold)/2 + extra + 1;
-	  //std::cout << "--- " << inew << " " << iold << " " << itmp << std::endl;
+		itmp = inew + dist;
+	  
 	  iold = inew;
 	  inew = itmp;
 	}
-  return inew-1;
+  return inew;
 }
 
 //A. O ataque ao Mosteiro da Batalha
 int main(int argc, char *argv[])
 {
-  int N, Q; cin >> N >> Q;
-  vector<int> alt(N), pot(Q);
-  for(int i = 0; i<N; ++i) cin >> alt[i];
-  for(int i = 0; i<Q; ++i) cin >> pot[i];
+  ll N, Q; cin >> N >> Q;
+  vector<ll> alt(N), pot(Q);
+  for(ll i = 0; i<N; ++i) cin >> alt[i];
+  for(ll i = 0; i<Q; ++i) cin >> pot[i];
 
-  for(int i = 0; i<Q; ++i) {
+  for(ll i = 0; i<Q; ++i) {
 	cout << logar(alt, pot[i]) << endl;
   }
   
