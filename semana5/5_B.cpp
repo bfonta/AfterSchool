025af831deab2e66b58dca3317859d8721730c98
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-12-03 14:47:08 +0100

    :white_check_mark: 5B

--- semana5/5_B.cpp
+++ semana5/5_B.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
+using ll = long long;
 
 //B. Problema B
 int main(int argc, char *argv[])
 {
   int N, Q; cin >> N >> Q;
-  vector<int> E(N), K(Q);
-  vector<int> partsum(N+1, 0);
-  for(int i = 0; i<N; ++i) cin >> E[i];
-  for(int i = 0; i<Q; ++i) cin >> K[i];
+  vector<ll> E(N), K(Q);
+  vector<ll> partsum(N+1, 0);
+  for(ll i = 0; i<N; ++i) cin >> E[i];
+  for(ll i = 0; i<Q; ++i) cin >> K[i];
 
   partsum[0] = 0;
-  for(int i = 1; i<=N; ++i) {
+  for(ll i = 1; i<=N; ++i) {
 	partsum[i] = partsum[i-1] + E[i-1];
-	std::cout << partsum[i] << std::endl;
   }
-  std::exit(0);
-  // for(int i = 0; i<Q; ++i) {
 
-  // 	  vector<int>::iterator lower_Bound;
-  // lower_Bound = lower_bound(v.begin(), v.end(), escolhido);
-
-  // int resposta = lower_Bound - v.begin();}
-  
+  vector<ll>::iterator lb;
+  for(ll i = 0; i<Q; ++i) {
+	lb = lower_bound(partsum.begin(), partsum.end(), K[i],
+					 [](ll elem, ll val){return elem <= val;});
+	cout << lb - partsum.begin() - 1 << " ";
+  }
+  cout << endl;
   return 0;
 }
